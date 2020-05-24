5ec7f918640888c0070c265f4efae42dc6ee7cea
gabriell <gabriel@smit.net.br>
2020-05-24 20:19:05 -0300

    Primeira versão

---
+++ NotificationSample/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace NotificationSample
+{
+    public static class AppConstants
+    {
+        public static string NotificationChannelName { get; set; } = "XamarinNotifyChannel";
+        public static string NotificationHubName { get; set; } = "[get from Azure]";
+        public static string ListenConnectionString { get; set; } = "[get from Azure]";
+        public static string DebugTag { get; set; } = "XamarinNotify";
+        public static string[] SubscriptionTags { get; set; } = { "default" };
+        public static string FCMTemplateBody { get; set; } = "{\"data\":{\"message\":\"$(messageParam)\"}}";
+        public static string APNTemplateBody { get; set; } = "{\"aps\":{\"alert\":\"$(messageParam)\"}}";
+    }
+}
---
+++ NotificationSample/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NotificationSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void AddMessage(string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if (messageDisplay.Children.OfType<Label>().Where(c => c.Text == message).Any())
+                {
+                    // Do nothing, an identical message already exists
+                }
+                else
+                {
+                    Label label = new Label()
+                    {
+                        Text = message,
+                        HorizontalOptions = LayoutOptions.CenterAndExpand,
+                        VerticalOptions = LayoutOptions.Start
+                    };
+                    messageDisplay.Children.Add(label);
+                }
+            });
+        }
+    }
+}
